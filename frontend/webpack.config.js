d9e1341dc88520285dc19867c483ab786748f995
Joao Neiva <joaoneivanetos@gmail.com>
2021-09-27 14:01:28 -0300

    rest quase completa

---
+++ frontend/webpack.config.js
@@ -0,0 +1,24 @@
+const path   = require('path');
+
+module.exports ={
+  
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output:{
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bunble.js'
+  },
+  devServer: {
+    static: path.resolve(__dirname,'public'),
+  },
+  module:{
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use:{
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  }
+};
